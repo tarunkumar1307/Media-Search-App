6f9f1182bcf3d3c86d8f8f6464423ce0148d86f3
Aadi <sskumartarun@gmail.com>
2026-04-04 16:36:22 +0530

    Add uiSlice for theme management

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import searchReducer from "./features/searchSlice";
 import collectionSlice from "./features/collectionSlice";
+import uiSlice from "./features/uiSlice"
 
 export const store = configureStore({
     reducer:{
         search: searchReducer,
-        collection: collectionSlice
+        collection: collectionSlice,
+        ui: uiSlice
     }
 })
