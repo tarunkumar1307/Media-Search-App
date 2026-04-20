d52c29854a24543f42a63338c3762d8664d61efd
Aadi <sskumartarun@gmail.com>
2026-04-20 16:20:56 +0530

    feat: add infinite scroll using react-infinite-scroll-component with API pagination

--- src/api/mediaAPI.js
+++ src/api/mediaAPI.js
@@ -4,7 +4,7 @@ const  UNSPLASH_KEY = import.meta.env.VITE_UNSPLASH_KEY
 const PEXELS_KEY = import.meta.env.VITE_PEXELS_KEY
 const GIPHY_KEY = import.meta.env.VITE_GIPHY_KEY
 
-export async function fetchPhotos(query, page=1, per_page=20){
+export async function fetchPhotos(query, page, per_page = 20) {
     const res = await axios.get('https://api.unsplash.com/search/photos', {
         params:{query, page, per_page},
         headers:{Authorization: `Client-ID ${UNSPLASH_KEY}`}
@@ -12,19 +12,20 @@ export async function fetchPhotos(query, page=1, per_page=20){
     return res.data
 }
 
-export async function fetchVideos(query, per_page=20){
+export async function fetchVideos(query, page, per_page = 20) {
     const res = await axios.get('https://api.pexels.com/videos/search', {
-        params: {query, per_page},
+        params: { query, page, per_page },
         headers: {Authorization: PEXELS_KEY}
     })
     return res.data
 }
 
-export async function fetchGIF(query, per_page=20){
+export async function fetchGIF(query, page, per_page = 20) {
     const res = await axios.get('https://api.giphy.com/v1/gifs/search', {
         params : {
             q: query,
             limit: per_page,
+            offset: (page - 1) * per_page,
             api_key: GIPHY_KEY
         }
     })
--- src/components/ResultGrid.jsx
+++ src/components/ResultGrid.jsx
@@ -1,11 +1,15 @@
 import { useDispatch, useSelector } from "react-redux"
 import { fetchGIF, fetchPhotos, fetchVideos } from "../api/mediaAPI"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { setError, setLoading, setResults } from "../redux/features/searchSlice"
 import ResultCard from "./ResultCard"
+import InfiniteScroll from "react-infinite-scroll-component";
 
 
 const ResultGrid = () => {
+    const [page, setPage] = useState(1)
+    const [hasMore, setHasMore] = useState(true)
+
     const dispatch = useDispatch()
     const { query, activeTab, results, loading, error } = useSelector((store) => store.search)
 
@@ -18,7 +22,10 @@ const ResultGrid = () => {
     }
 
     useEffect(() => {
-        if(!query) return
+        if (!query) return
+
+        setPage(1)
+        setHasMore(true)
 
         const getData = async () => {
 
@@ -27,7 +34,7 @@ const ResultGrid = () => {
                 let data
 
                 if (activeTab == 'all') {
-                    let [photoRes, videoRes, gifRes] = await Promise.all([fetchPhotos(query), fetchVideos(query), fetchGIF(query)])
+                    let [photoRes, videoRes, gifRes] = await Promise.all([fetchPhotos(query, 1), fetchVideos(query, 1), fetchGIF(query, 1)])
 
                     let photos = photoRes.results.map((item) => ({
                         id: item.id,
@@ -67,7 +74,7 @@ const ResultGrid = () => {
                 }
 
                 else if (activeTab == 'photo') {
-                    let res = await fetchPhotos(query)
+                    let res = await fetchPhotos(query, 1)
                     data = res.results.map((item) => ({
                         id: item.id,
                         type: 'photo',
@@ -80,7 +87,7 @@ const ResultGrid = () => {
                 }
 
                 else if (activeTab == 'video') {
-                    let res = await fetchVideos(query)
+                    let res = await fetchVideos(query, 1)
                     data = res.videos.map((item) => ({
                         id: item.id,
                         type: 'video',
@@ -91,7 +98,7 @@ const ResultGrid = () => {
                     }))
                 }
                 else if (activeTab == 'GIF') {
-                    let res = await fetchGIF(query)
+                    let res = await fetchGIF(query, 1)
                     data = res.data.map((item) => ({
                         id: item.id,
                         type: 'GIF',
@@ -110,17 +117,118 @@ const ResultGrid = () => {
 
     }, [query, activeTab])
 
-    if(error) return <h1>Error</h1>
-    if(loading) return <h1>Loading...</h1>
+    const fetchMoreData = async () => {
+        if (loading) return
+
+        setPage((prev) => prev + 1)
+
+        try {
+            let data
+
+            if (activeTab == 'all') {
+                let [photoRes, videoRes, gifRes] = await Promise.all([fetchPhotos(query, 1), fetchVideos(query, 1), fetchGIF(query, 1)])
+
+                let photos = photoRes.results.map((item) => ({
+                    id: item.id,
+                    type: 'photo',
+                    thumbnail: item.urls.small,
+                    title: item.alt_description,
+                    src: item.urls.full,
+                    url: item.links.html
+                }))
+
+                // let videos = videoRes.videos.map((item) => ({
+                //     id: item.id,
+                //     type: 'video',
+                //     thumbnail: item.image,
+                //     title: item.user.name || 'Video',    
+                //     src: item.video_files[0].link,
+                //     url: item.url
+                // }))
+
+                let gifs = gifRes.data.map((item) => ({
+                    id: item.id,
+                    type: 'GIF',
+                    thumbnail: item.images.original.url,
+                    title: item.title,
+                    src: item.images.fixed_height.url,
+                    url: item.url
+                }))
+
+                const minLength = Math.min(photos.length, gifs.length)
+                let mixed = []
+
+                for (let i = 0; i < minLength; i++) {
+                    mixed.push(photos[i], gifs[i])
+                }
+
+                data = shuffleArray(mixed)
+            }
+            else if (activeTab == 'photo') {
+                let res = await fetchPhotos(query, page)
+                data = res.results.map((item) => ({
+                    id: item.id,
+                    type: 'photo',
+                    thumbnail: item.urls.small,
+                    title: item.alt_description,
+                    src: item.urls.full,
+                    url: item.links.html
+                }))
+            }
+            else if (activeTab == 'video') {
+                let res = await fetchVideos(query, page)
+                data = res.videos.map((item) => ({
+                    id: item.id,
+                    type: 'video',
+                    thumbnail: item.image,
+                    title: item.user.name || 'Video',
+                    src: item.video_files[0].link,
+                    url: item.url
+                }))
+            }
+            else if (activeTab == 'GIF') {
+                let res = await fetchGIF(query, page)
+                data = res.data.map((item) => ({
+                    id: item.id,
+                    type: 'GIF',
+                    thumbnail: item.images.original.url,
+                    title: item.title,
+                    src: item.images.fixed_height.url,
+                    url: item.url
+                }))
+            }
+
+            if (!data || data.length === 0) {
+                setHasMore(false)
+                return
+            }
+
+            dispatch(setResults([...results, ...data]))
+        } catch (err) {
+            dispatch(setError(err.message))
+        }
+    }
+
+    if (error) return <h1>Error</h1>
+    if (loading) return <h1>Loading...</h1>
+
+
 
     return (
-        <div className='columns-2 sm:columns-3 md:columns-4 lg:columns-5 gap-x-5 gap-y-5 p-6'>
-            {results.map((item) => (
-                <div key={item.id} className="mb-5 break-inside-avoid">
-                    <ResultCard item={item} />
-                </div>
-    ))}
-        </div>
+        <InfiniteScroll
+            dataLength={results.length}
+            next={fetchMoreData}
+            hasMore={hasMore}
+            loader={<h4 className="text-center">Loading...</h4>}
+        >
+            <div className='columns-2 sm:columns-3 md:columns-4 lg:columns-5 gap-x-5 gap-y-5 p-6'>
+                {results.map((item) => (
+                    <div key={item.id} className="mb-5 break-inside-avoid">
+                        <ResultCard item={item} />
+                    </div>
+                ))}
+            </div>
+        </InfiniteScroll>
     )
 }
 
